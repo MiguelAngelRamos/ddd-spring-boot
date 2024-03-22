a5791e80046f2aac4ff4a9ac73119f52fc909817
Miguel Angel Ramos <return.infox@gmail.com>
2024-03-22 17:28:50 -0300

    productos service application

--- src/main/java/com/kibernumacademy/crud/application/service/ProductApplicationService.java
+++ src/main/java/com/kibernumacademy/crud/application/service/ProductApplicationService.java
@@ -1,5 +1,37 @@
 package com.kibernumacademy.crud.application.service;
 
+import org.springframework.stereotype.Service;
+
+import com.kibernumacademy.crud.application.dto.ProductDTO;
+import com.kibernumacademy.crud.domain.model.Product;
+import com.kibernumacademy.crud.domain.service.ProductService;
+
+@Service
 public class ProductApplicationService {
   
+  private final ProductService productService;
+
+  public ProductApplicationService(ProductService productService) {
+    this.productService = productService;
+  }
+  
+  public ProductDTO createProduct(ProductDTO productDTO) {
+    Product product = convertToEntity(productDTO);
+    Product saveProduct = productService.saveOrUpdateProduct(product);
+
+    return convertToDTO(saveProduct);
+  }
+
+  private Product convertToEntity(ProductDTO productDTO) {
+    return new Product(productDTO.getName(), productDTO.getPrice());
+  }
+
+  private ProductDTO convertToDTO(Product product) {
+    return new ProductDTO(product.getId(), product.getName(), product.getPrice());
+  }
+
+
+
+  
+
 }
