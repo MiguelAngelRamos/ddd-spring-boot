8997b6d22e0fcdc416a75c3191e5236dc4402900
Miguel Angel Ramos <return.infox@gmail.com>
2024-03-22 18:01:45 -0300

    application terminada

--- src/main/java/com/kibernumacademy/crud/application/service/ProductApplicationService.java
+++ src/main/java/com/kibernumacademy/crud/application/service/ProductApplicationService.java
@@ -6,6 +6,12 @@ import com.kibernumacademy.crud.application.dto.ProductDTO;
 import com.kibernumacademy.crud.domain.model.Product;
 import com.kibernumacademy.crud.domain.service.ProductService;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
+
 @Service
 public class ProductApplicationService {
   
@@ -14,7 +20,7 @@ public class ProductApplicationService {
   public ProductApplicationService(ProductService productService) {
     this.productService = productService;
   }
-  
+
   public ProductDTO createProduct(ProductDTO productDTO) {
     Product product = convertToEntity(productDTO);
     Product saveProduct = productService.saveOrUpdateProduct(product);
@@ -22,6 +28,33 @@ public class ProductApplicationService {
     return convertToDTO(saveProduct);
   }
 
+  public Optional<ProductDTO> getProductById(Long id) {
+    return productService.getProductById(id).map(this::convertToDTO);
+  }
+
+  public List<ProductDTO> listAllProducts() {
+    return productService.listAllProducts().stream().map(this::convertToDTO).collect(Collectors.toList());
+  }
+
+  // http://localhost:8080/api/10
+  public ProductDTO updateProduct(Long id, ProductDTO productDTO) {
+    Optional<Product> existingProduct = productService.getProductById(id);
+
+    if(!existingProduct.isPresent()) throw new EntityNotFoundException("Product not with ID: " +  id);
+  
+      Product productToUpdate = existingProduct.get();
+      productToUpdate.setName(productDTO.getName());
+      productToUpdate.setPrice(productDTO.getPrice());
+      Product updatedProduct = productService.saveOrUpdateProduct(productToUpdate);
+      return convertToDTO(updatedProduct);
+   
+  }
+
+  public void deleteProductById(Long id) {
+    productService.deleteProductById(id);
+  }
+  
+  // Transaccion de datos entre capas
   private Product convertToEntity(ProductDTO productDTO) {
     return new Product(productDTO.getName(), productDTO.getPrice());
   }
