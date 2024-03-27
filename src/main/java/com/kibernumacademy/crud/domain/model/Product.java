d7e118c3fb484711d7eff94b3ee7119654433912
Miguel Angel Ramos <return.infox@gmail.com>
2024-03-27 16:12:09 -0300

    correcion del modelo

--- src/main/java/com/kibernumacademy/crud/domain/model/Product.java
+++ src/main/java/com/kibernumacademy/crud/domain/model/Product.java
@@ -3,10 +3,15 @@ package com.kibernumacademy.crud.domain.model;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Product {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
   private String name;
   private BigDecimal price;
